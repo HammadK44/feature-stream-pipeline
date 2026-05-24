fd1e606800960f652d8d3452d5742087d9ce0d45
Hammad Khalid <hammadbink@gmail.com>
2026-05-24 15:07:00 +0000

    joining loan and payment tables into silver layer

---
+++ src/feat_stream/ingestion/silver.py
@@ -0,0 +1,41 @@
+import polars as pl
+
+from feat_stream.config import settings
+from feat_stream.storage import s3
+
+
+LOAN_RENAMES = {
+    'id': 'loan_id',
+    'amount': 'loan_amount',
+    'status': 'loan_status',
+    'created_on': 'loan_created_on',
+    'updated_on': 'loan_updated_on',
+    'matured_on': 'loan_matured_on',
+}
+
+PAYMENT_RENAMES = {
+    'id': 'payment_id',
+    'amount': 'payment_amount',
+    'status': 'payment_status',
+    'created_on': 'payment_created_on',
+}
+
+
+def _read_bronze_table(fs, table):
+    keys = s3.list_parquet_keys(settings.s3_bucket_bronze, f'{table}/')
+    frames = []
+    for key in keys:
+        with fs.open_input_file(f'{settings.s3_bucket_bronze}/{key}') as f:
+            frames.append(pl.read_parquet(f))
+    return pl.concat(frames)
+
+
+def build_loan_payments():
+    fs = s3.fs()
+    loans = _read_bronze_table(fs, 'loan').rename(LOAN_RENAMES)
+    payments = _read_bronze_table(fs, 'payment').rename(PAYMENT_RENAMES)
+    joined = loans.join(payments, on='loan_id', how='left')
+    key = f'{settings.s3_bucket_silver}/loan_payments/data.parquet'
+    with fs.open_output_stream(key) as out:
+        joined.write_parquet(out)
+    return joined.height
